6270fe7014d076217f8190eebead870798d674ab
Jack Ketcham <jack@jackketcham.com>
2024-12-27 13:37:58 -0700

    style: Adjust vertial alignment in footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -174,7 +174,8 @@ export default function Footer() {
         </div>
         <div className="mt-20 text-center text-sm">
           <p>
-            © 2024 Tempe Bicycle Action Group
+            <span className="align-text-top">&copy;</span> 2024 Tempe Bicycle
+            Action Group
             <span className="hidden sm:inline"> · </span>
             <br className="sm:hidden" />
             All rights reserved
